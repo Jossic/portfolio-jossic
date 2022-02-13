a9e39fabab70f6defe15fb8b39f6b51187dbd3e5
Jossic <jossic.lapierre@gmail.com>
2022-02-13 22:42:31 +0100

    About page + data

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -6,7 +6,7 @@ const AboutMe = () => {
 		<section className='bg-white dark:bg-gray-800'>
 			<div className='max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800'>
 				<h1 className=' text-5xl md:text-9xl font-bold py-20 text-center md:text-left'>
-					About Me.
+					A propos de moi.
 				</h1>
 			</div>
 			<div className='bg-[#F1F1F1] -mt-10 dark:bg-gray-900'>
@@ -14,7 +14,7 @@ const AboutMe = () => {
 					<p
 						className='leading-loose text-2xl md:text-4xl font-semibold  mx-4'
 						style={{ lineHeight: '3rem' }}>
-						{userData.about.title}. Currently working on{' '}
+						{userData.about.title}. Je travaille sur{' '}
 						<a
 							className='bg-red-500 rounded-md px-2 py-1 text-white'
 							href={userData.about.currentProjectUrl}>
@@ -24,7 +24,7 @@ const AboutMe = () => {
 				</div>
 			</div>
 			<div className='bg-[#F1F1F1] dark:bg-gray-900 px-4'>
-				<div className='pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20'>
+				<div className=' pt-20 grid grid-cols-1 md:grid-cols-3 max-w-6xl mx-auto gap-y-20 gap-x-20 '>
 					{/* Social Buttons */}
 					<div className='inline-flex flex-col'>
 						<div>
@@ -32,34 +32,43 @@ const AboutMe = () => {
 								Contact
 							</h1>
 							<p className='text-lg text-gray-500 mt-4 dark:text-gray-300'>
-								For any sort help / enquiry, shoot a{' '}
+								Pour toutes demandes ou questions, n'hésitez pas
+								à m'écrire par{' '}
 								<a
 									href={`mailto:${userData.email}`}
 									className='text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300'>
 									mail
 								</a>{' '}
-								and I'll get back. I swear.
+								et je reviendrais vers vous.
 							</p>
 						</div>
 						<div className='mt-8'>
 							<h1 className='text-xl font-semibold text-gray-700 dark:text-gray-200'>
-								Job Opportunities
+								Emploi
 							</h1>
 							<p className='text-lg text-gray-500 mt-4 dark:text-gray-300'>
-								I'm looking for a job currently, If you see me
-								as a good fit, check my{' '}
+								Je suis actuellement à la recherche d'une
+								mission ou d'un contrat, vous pouvez consulter
+								mon{' '}
 								<a
-									href={userData.resumeUrl}
+									href={userData.resumeUrlFR}
 									target='__blank'
 									className='text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300'>
-									CV
+									CV en version FR
 								</a>{' '}
-								and I'd love to work for you.
+								ou{' '}
+								<a
+									href={userData.resumeUrlEN}
+									target='__blank'
+									className='text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300'>
+									en version EN
+								</a>{' '}
+								et je donnerais mon maximum pour vous.
 							</p>
 						</div>
 						{/* Social Links */}
 						<h1 className='text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200'>
-							Social Links
+							Réseaux sociaux
 						</h1>
 						<div className='mt-4 ml-4'>
 							<div className='flex flex-row justify-start items-center '>
@@ -106,17 +115,46 @@ const AboutMe = () => {
 									</p>
 								</a>
 							</div>
-							<div className='flex flex-row justify-start items-center'>
-								<a
-									href={userData.socialLinks.twitter}
-									className='flex flex-row items-center space-x-4 group'>
-									<div className='my-4'>&rarr;</div>
-									<p className='text-lg text-gray-500 font-mono relative overflow-hidden dark:text-gray-300'>
-										<div className='absolute h-0.5 w-full bg-gray-400 bottom-0 transform -translate-x-28 group-hover:translate-x-0 transition duration-300'></div>
-										Instagram
-									</p>
-								</a>
-							</div>
+						</div>
+						<h1 className='text-xl font-semibold text-gray-700 mt-8 dark:text-gray-200'>
+							Tech Stack
+						</h1>
+
+						<div className='flex flex-row flex-wrap mt-8'>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
+
+							<img
+								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png'
+								className='h-16 w-16 mx-3 my-3'
+							/>
 						</div>
 					</div>
 					{/* Text area */}
@@ -128,60 +166,6 @@ const AboutMe = () => {
 								{desc}
 							</p>
 						))}
-
-						<h1 className='bg-red-500 text-3xl rounded-md px-2 py-1 inline-block font-bold text-gray-50'>
-							Tech Stack
-						</h1>
-						<div className='flex flex-row flex-wrap mt-8'>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/java/java.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/html/html.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/angular/angular.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/vue/vue.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/firebase/firebase.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mysql/mysql.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-							<img
-								src='https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/mongodb/mongodb.png'
-								className='h-20 w-20 mx-4 my-4'
-							/>
-						</div>
 					</div>
 				</div>
 			</div>
