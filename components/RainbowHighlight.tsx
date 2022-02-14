a5af4936bf64f55bd6d23fea830c45e5cdbcb726
Jossic <jossic.lapierre@gmail.com>
2022-02-14 21:19:51 +0100

    Update rainbow type

--- components/RainbowHighlight.tsx
+++ components/RainbowHighlight.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { RoughNotation } from 'react-rough-notation';
 
-export const RainbowHighlight = ({ color, children }) => {
+interface Rainbow {
+	color: string;
+	children: React.ReactChild[];
+}
+
+export const RainbowHighlight = ({ color, children }: Rainbow) => {
 	// Change the animation duration depending on length of text we're animating (speed = distance / time)
 	const animationDuration = Math.floor(30 * children.length);
 
